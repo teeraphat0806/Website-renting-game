c70674d3067a2879a6b191cc12b0054234993776
gtx550000 <114909099+gtx550000@users.noreply.github.com>
2024-01-10 22:53:11 +0700

    sing_in_react_Waiting to fix_10/1/24

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,73 @@
 import { useState } from 'react'
-import ButtonAppBar from './components/navber/navber'
-import TemporaryDrawer from './components/sidebar/sidebar'
-import Logout from './components/logout/logout'
-import Home from './components/home/home'
-import Profile from './components/profile/profile'
-
-import { BrowserRouter as Router , Routes , Route } from 'react-router-dom';
+import Bgvideo from '../src/media/VN.mp4'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
 
 function App() {
+  /*const container = document.getElementById('container');
+  const registerBtn = document.getElementById('register');
+  const loginBtn = document.getElementById('login');
+  
+  registerBtn.addEventListener('click', () => {
+      container.classList.add("active");
+  });
+  
+  loginBtn.addEventListener('click', () => {
+      container.classList.remove("active");
+  });*/
+
   return (
-    <Router>
-   <div>
-    <ButtonAppBar></ButtonAppBar>
-    <Routes>
-       <Route path='/logout' element={<Logout/>}></Route>
-       <Route path='/home' element={<Home/>}></Route>
-       <Route path='/profile' element={<Profile/>}></Route>
-    </Routes>   
-   </div>
-   </Router>
+  <body>
+  
+      <video autoPlay loop muted  className="back-video">
+      <source src={Bgvideo} type='video/mp4'/>
+      </video>
+    
+    <div className="container" id="container">
+
+          <div className="form-container sign-up">
+               <form>
+                   <h1>Create Account</h1>
+                   <span>or use your email for registeration</span>
+                   <input type="text" placeholder="Name"/>
+                   <input type="email" placeholder="Email"/>
+                   <input type="password" placeholder="Password"/>
+                   <button>Sign Up</button>
+               </form>
+          </div>
+
+          <div className="form-container sign-in">
+                <form>
+                    <h1>Sign In</h1>
+                    <span>or use your email password</span>
+                    <input type="email" placeholder="Email"/>
+                    <input type="password" placeholder="Password"/>
+                    <a href="#">Forget Your Password?</a>
+                    <button>Sign In</button>
+               </form>
+          </div>
+
+          <div className="toggle-container">
+            <div className="toggle">
+
+                <div className="toggle-panel toggle-left">
+                    <h1>Welcome!</h1>
+                    <p>Enter your personal details to use all</p>
+                    <button className="hidden" id="login">Sign In</button>
+                </div>
+
+                <div className="toggle-panel toggle-right">
+                    <h1>Hello, Friend!</h1>
+                    <p>Register and joy me!</p>
+                    <button className="hidden" id="register">Sign Up</button>
+                </div>
+
+            </div>
+        </div>
+
+    </div>
+  </body>
   )
 }
 
-export default App
+export default App;
---
+++ src/components/mainpage/mainpage.jsx
@@ -0,0 +1,105 @@
+import React from "react";
+import '../mainpage/main.css';
+function Main() {
+ 
+
+    return (
+         <div>
+          <main>
+  <ul className='slider'>
+    <li className='item' style="background-image: url('https://cdn.mos.cms.futurecdn.net/dP3N4qnEZ4tCTCLq59iysd.jpg')">
+      <div className='content'>
+        <h2 className='title'>"Lossless Youths"</h2>
+        <p className='description'> Lorem ipsum, dolor sit amet consectetur
+        adipisicing elit. Tempore fuga voluptatum, iure corporis inventore
+        praesentium nisi. Id laboriosam ipsam enim.  </p>
+        <button>Read More</button>
+      </div>
+    </li>
+    <li className='item' style="background-image: url('https://i.redd.it/tc0aqpv92pn21.jpg')">
+      <div className='content'>
+        <h2 className='title'>"Estrange Bond"</h2>
+        <p className='description'> Lorem ipsum, dolor sit amet consectetur
+        adipisicing elit. Tempore fuga voluptatum, iure corporis inventore
+        praesentium nisi. Id laboriosam ipsam enim.  </p>
+        <button>Read More</button>
+      </div>
+    </li>
+    <li className='item' style="background-image: url('https://wharferj.files.wordpress.com/2015/11/bio_north.jpg')">
+      <div className='content'>
+        <h2 className='title'>"The Gate Keeper"</h2>
+        <p className='description'> Lorem ipsum, dolor sit amet consectetur
+        adipisicing elit. Tempore fuga voluptatum, iure corporis inventore
+        praesentium nisi. Id laboriosam ipsam enim.  </p>
+        <button>Read More</button>
+      </div>
+    </li>
+    <li className='item' style="background-image: url('https://images7.alphacoders.com/878/878663.jpg')">
+      <div className='content'>
+        <h2 className='title'>"Last Trace Of Us"</h2>
+        <p className='description'>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore fuga voluptatum, iure corporis inventore praesentium nisi. Id laboriosam ipsam enim.
+        </p>
+        <button>Read More</button>
+      </div>
+    </li>
+    <li className='item' style="background-image: url('https://theawesomer.com/photos/2017/07/simon_stalenhag_the_electric_state_6.jpg')">
+      <div className='content'>
+        <h2 className='title'>"Urban Decay"</h2>
+        <p className='description'>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore fuga voluptatum, iure corporis inventore praesentium nisi. Id laboriosam ipsam enim.
+        </p>
+        <button>Read More</button>
+      </div>
+    </li>
+    <li className='item' style="background-image: url('https://da.se/app/uploads/2015/09/simon-december1994.jpg')">
+      <div className='content'>
+        <h2 className='title'>"The Migration"</h2>
+        <p className='description'> Lorem ipsum, dolor sit amet consectetur
+        adipisicing elit. Tempore fuga voluptatum, iure corporis inventore
+        praesentium nisi. Id laboriosam ipsam enim.  </p>
+        <button>Read More</button>
+      </div>
+    </li>
+  </ul>
+  <nav className='nav'>
+    <ion-icon className='btn prev' name="arrow-back-outline"></ion-icon>
+    <ion-icon className='btn next' name="arrow-forward-outline"></ion-icon>
+  </nav>
+</main>
+         </div>
+    )
+  }
+  
+  export default Main;
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
